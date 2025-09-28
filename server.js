e747e60fe3d3f44028b6a1e7e3aa7546c830a62d
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2025-09-28 17:06:35 +0530

    maza commit

--- server.js
+++ server.js
@@ -8,18 +8,19 @@ const PORT = process.env.PORT || 3000;
 // Example env var
 const GREETING = process.env.GREETING || "Hello from minimal Node app!";
 const CODER = process.env.CODER || "env not loaded!";
+const COUNT = process.env.COUNT || "env not loaded!";
 
 // Root route
 app.get('/', (req, res) => {
   res.send(`<h1>${GREETING}</h1><p>Running on port ${PORT}</p>`);
 });
-
+  
 // Health check (good for Render)
 app.get('/healthz', (req, res) => res.send("ok"));
 
 app.get('/programmer', (req, res) => res.send("Hello, World Programmer"));
 
-app.get('/coder', (req, res) => res.send(`${CODER}`));
+app.get('/coder', (req, res) => res.send(`${CODER} and repository count: ${COUNT}`));
 
 app.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
